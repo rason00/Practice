81d0d72a47840130698c5cadfd19c068b89cf260
Rason <rason00@163.com>
2017-11-15 10:54:55 +0800

    添加各浏览器

--- Webpack/postcss.config.js
+++ Webpack/postcss.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: [require('autoprefixer')]
+  plugins: {
+    'autoprefixer': {
+      browsers: [
+        'iOS >= 7', 
+        'Android >= 4.1',
+        'last 10 Chrome versions',
+        'last 10 Firefox versions',
+        'Safari >= 6',
+        'ie > 8'
+      ]
+    }
+  }
 }
