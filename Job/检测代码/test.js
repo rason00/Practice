6552ae73b3c81ecf4f19215f3bf76e8a149046a8
Rason <rason00@163.com>
2017-12-02 10:35:55 +0800

    Add files via upload

---
+++ Job/检测代码/test.js
@@ -0,0 +1,31 @@
+<script type='text/javascript'>
+  var _gsq = _gsq || [];
+        (function () {
+            var s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.async = true;
+            s.src = (location.protocol == 'https:' ? 'https://ssl.' : 'http://static.') + 'gridsumdissector.com/js/Clients/GWD-002942-165430/gs.js';
+            var firstScript = document.getElementsByTagName('script')[0];
+            firstScript.parentNode.insertBefore(s, firstScript);
+        })();
+    </script>
+
+_gsq.push(['T', 'GWD-002942', 'trackEvent', 'click', '撩你的年终奖', location.pathname]);
+
+var leadsID = "sqtywl" + new Date().getTime() + "-" + Math.floor(Math.random() * 1000);
+if (window.gsTracker) {
+  var orderid = leadsID;
+  gsTracker.addOrder(orderid, 1);
+  gsTracker.setEcomProperty(orderid, "1", _name);
+  gsTracker.setEcomProperty(orderid, "2", _phone);
+  gsTracker.setEcomProperty(orderid, "3", "宝骏510");
+  gsTracker.setEcomProperty(orderid, "4", province_name);
+  gsTracker.setEcomProperty(orderid, "5", city_name);
+  gsTracker.setEcomProperty(orderid, "6", dealer_name);
+  gsTracker.addProduct(orderid, location.pathname, location.pathname, 1, 1, "宝骏510网络集合");
+  //此处可以根据活动页面实际title进行更换；
+  gsTracker.trackECom();
+  gsTracker.track("/targetpage/formsubmit/sqtywlpc");
+}
+_smq.push(['custom', '17-baojun', 'Baojun510NZJM-submitsuccess', '{' + _name + '+' + _phone + '+' + province_name + '+' + city_name + '+' + leadsID + '}']);
+
